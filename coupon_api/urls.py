e1f23475d78c472546d0c79fbe4dad7820bf0e02
dev-code0101 <115885392+dev-code0101@users.noreply.github.com>
2023-07-19 08:06:44 +0530

    Add files via upload

--- coupon_api/urls.py
+++ coupon_api/urls.py
@@ -19,9 +19,11 @@ from django.urls import path, include
 
 from django.conf import settings
 from django.conf.urls.static import static
+from .views import index
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/coupons/', include('coupons.urls')),
+    path('', index),
 ]
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
